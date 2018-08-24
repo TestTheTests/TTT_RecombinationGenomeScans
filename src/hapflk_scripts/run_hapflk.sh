4db517fd51371e907862b9b8ff1f412ba6340ba8
Kevin Freeman <freeman.k@husky.neu.edu>
2018-08-24 16:16:26 -0400

    modify to be used without the src directory --- I felt that having 2 nested src directories would get confusing

--- src/hapflk_scripts/run_hapflk.sh
+++ src/hapflk_scripts/run_hapflk.sh
@@ -9,7 +9,7 @@
 # run_hapflk.sh is a script that runs hapflk analysis on a set
 # of VCFs. It should be run in a folder containing the 1) VCFs 
 # 2) "scan results files" indicating independence (for pruning)
-# and 3) a "src" subfolder that contains the following scripts:
+# and 3) The following scripts:
 #   - vcf2plink.pl
 #   - splitPedMapbyChrom.pl
 #   - pruneSNPs.pl
@@ -55,14 +55,14 @@ do
 		continue
 	fi
 	# create pruned file
-	${myDir}/src/pruneSNPs.pl -vcf $curVCF -scan $myDir/${i}_${simType}_"ScanResults.txt" -outfile $curPruned
+	${myDir}/pruneSNPs.pl -vcf $curVCF -scan $myDir/${i}_${simType}_"ScanResults.txt" -outfile $curPruned
 	
 	echo -e "\nconverting to PLINK format\n"
 	
 	allPlink="${myDir}/plink_files/${i}/${i}_${simType}_VCFallFILT"
 	prunedPlink="${myDir}/plink_files/${i}/${i}_PRUNED_${simType}_VCFallFILT"
-	numGroups=$(perl ${myDir}/src/vcf2plink.pl -vcf $curVCF -pop $curPop -outfile $allPlink 2>>logfiles/$i.log &)
-	perl ${myDir}/src/vcf2plink.pl -vcf $curPruned -pop $curPop -outfile $prunedPlink 2>>logfiles/$i.log &
+	numGroups=$(perl ${myDir}/vcf2plink.pl -vcf $curVCF -pop $curPop -outfile $allPlink 2>>logfiles/$i.log &)
+	perl ${myDir}/vcf2plink.pl -vcf $curPruned -pop $curPop -outfile $prunedPlink 2>>logfiles/$i.log &
 	 wait
 	
 	# generate a kinship file using all data
@@ -76,21 +76,21 @@ do
 	kinshipPruned="$myDir/kinship/${i}_PRUNED_fij.txt"
 	
 	# split files by chromosome
-	perl ${myDir}/src/splitPedMapbyChrom.pl -infile $allPlink -outfile "${myDir}/plink_files/${i}/${i}" >>logfiles/$i.log 2>&1 &
-	perl ${myDir}/src/splitPedMapbyChrom.pl -infile $prunedPlink -outfile "${myDir}/plink_files/${i}/${i}_PRUNED" >>logfiles/$i.log 2>&1 &
+	perl ${myDir}/splitPedMapbyChrom.pl -infile $allPlink -outfile "${myDir}/plink_files/${i}/${i}" >>logfiles/$i.log 2>&1 &
+	perl ${myDir}/splitPedMapbyChrom.pl -infile $prunedPlink -outfile "${myDir}/plink_files/${i}/${i}_PRUNED" >>logfiles/$i.log 2>&1 &
 	wait
 	
 	################# Main hafplk analysis #############################
 	### unpruned
-	bash ${myDir}/src/runHapflkAllChroms.sh ${myDir}/plink_files/${i} ${myDir}/results/ALL/$i $i $kinshipAll $K >>logfiles/$i.log 2>&1
+	bash ${myDir}/runHapflkAllChroms.sh ${myDir}/plink_files/${i} ${myDir}/results/ALL/$i $i $kinshipAll $K >>logfiles/$i.log 2>&1
 	# combine the results from the 10 chromosomes using an Rscript
-	Rscript ${myDir}/src/concat_chr.R "${myDir}/results/ALL/$i/${i}_chr" "${myDir}/results/ALL/" $i
+	Rscript ${myDir}/concat_chr.R "${myDir}/results/ALL/$i/${i}_chr" "${myDir}/results/ALL/" $i
 	### unpruned with pruned kinship
-	bash ${myDir}/src/runHapflkAllChroms.sh ${myDir}/plink_files/${i} ${myDir}/results/ALL_PRUNED_KIN/$i ${i} $kinshipPruned $K >>logfiles/$i.log 2>&1
-	Rscript ${myDir}/src/concat_chr.R "${myDir}/results/ALL_PRUNED_KIN/$i/${i}_chr" "${myDir}/results/ALL_PRUNED_KIN/" $i
+	bash ${myDir}/runHapflkAllChroms.sh ${myDir}/plink_files/${i} ${myDir}/results/ALL_PRUNED_KIN/$i ${i} $kinshipPruned $K >>logfiles/$i.log 2>&1
+	Rscript ${myDir}/concat_chr.R "${myDir}/results/ALL_PRUNED_KIN/$i/${i}_chr" "${myDir}/results/ALL_PRUNED_KIN/" $i
 	#### pruned with pruned kinship
-	bash ${myDir}/src/runHapflkAllChroms.sh ${myDir}/plink_files/${i} ${myDir}/results/PRUNED/$i ${i}_PRUNED $kinshipPruned $K >>logfiles/$i.log 2>&1
-	Rscript ${myDir}/src/concat_chr.R "${myDir}/results/PRUNED/$i/${i}_PRUNED_chr" "${myDir}/results/PRUNED/" $i
+	bash ${myDir}/runHapflkAllChroms.sh ${myDir}/plink_files/${i} ${myDir}/results/PRUNED/$i ${i}_PRUNED $kinshipPruned $K >>logfiles/$i.log 2>&1
+	Rscript ${myDir}/concat_chr.R "${myDir}/results/PRUNED/$i/${i}_PRUNED_chr" "${myDir}/results/PRUNED/" $i
 	echo -e "\n\nFinished $i. $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min into analysis"
 
 done
