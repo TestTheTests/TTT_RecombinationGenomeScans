a476855d7125c327ec66fedd926aa7821891b0af
Katie Lotterhos <k.lotterhos@gmail.com>
2019-01-29 09:41:33 -0500

    update baypass_results
    baypass was run with the wrong variable in the phenotype column, so results with "phenotype" were removed from the files

--- src/other_src/Script.sh
+++ src/other_src/Script.sh
@@ -8,6 +8,8 @@
 
 awk -F' ' '{print NF; exit}' results_final_old2/10900_Invers_ScanResults.txt
 
+
+#### FIX 1 ###################
 results_final_old2/10900_Invers_ScanResults.txt
 
 for i in {10900..11109}
@@ -20,6 +22,8 @@ awk '{print $44, $45, $46, $47, $48, $49, $50}' results_final_old2/${i}_Invers_S
 
 done
 
+
+#### FIX 2 ###################
 mkdir results_final_old3
 mv results_final/*Invers_ScanResults.txt results_final_old3
 # remove white space
@@ -37,4 +41,16 @@ for i in {10900..11109}
 do
 echo $i
 sed -e 's/^[ \t]*//' results_final_old3/${i}_Invers_scikitallel.txt > results_final/${i}_Invers_scikitallel.txt
+done
+
+
+#### FIX 3 ###################
+### Had to remove phenotype columns from Baypass file because these
+### were actually run with the environment and not the phenotype
+
+cd ~/Desktop/Repos/TestTheTests/TTT_RecombinationGenomeScans/results_baypass_old
+for i in {10900..11109}
+do
+echo $i
+awk '{print $1,$3,$4,$6,$7,$8, $10}' ${i}_baypass_results.txt > ../results_final/${i}_baypass_results.txt
 done
